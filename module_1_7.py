8c2380167da214f96e30cab22f67868a64060b66
Ivan515-cpu <ig13kuznetsov@gmail.com>
2024-12-14 00:06:46 +0500

    Add files via upload
    Строка 3 заремина, так как в противном случае программа выдаёт ошибку в связи с тем, что кортежи являются неизменяемыми объектами

---
+++ module_1_7.py
@@ -0,0 +1,9 @@
+immutable_var = (1, 9, 6, 1, 'year', 'абракадабра')
+print(immutable_var)
+# immutable_var[5]='смысл'
+print(immutable_var)
+mutable_list = [13, 12, 2024, 'John', 'Denis']
+mutable_list[0] = 9
+mutable_list[1] = 2
+mutable_list[2] = 1961
+print(mutable_list)
